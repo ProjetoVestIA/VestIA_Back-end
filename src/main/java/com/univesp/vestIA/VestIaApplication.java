79439738711f7a7e38e413153a3a8afd3cb2b2f3
SayuriCristina <sayuri.cristinass@gmail.com>
2025-10-02 20:23:50 -0300

    Init: Primeiro commit.

---
+++ src/main/java/com/univesp/vestIA/VestIaApplication.java
@@ -0,0 +1,13 @@
+package com.univesp.vestIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VestIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univesp/vestIA/VestIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univesp.vestIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
